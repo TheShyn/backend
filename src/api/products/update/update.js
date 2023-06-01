5bf2b8124e12a2e03a37c85770f4372cdca3eb59
Nguyen Trung Duc <huyga612@gmail.com>
2023-06-01 21:11:43 +0700

    update update product

--- src/api/products/update/update.js
+++ src/api/products/update/update.js
@@ -3,18 +3,19 @@ import Product from "../../../models/Product"
 import mongoose from "mongoose"
 import SchemaProduct from "../../../validate/SchemaProduct";
 import Categories from "../../../models/Categories";
-import cloudinary from 'cloudinary-core';
 import dotenv from 'dotenv'
+import SchemaProductUpdate from "../../../validate/SchemaProductUpdate";
+import { v2 as cloudinary } from 'cloudinary';
 dotenv.config()
 
-const cloudinaryApi = new cloudinary.Cloudinary({ cloud_name: process.env.CLOUDINARY_NAME });
 const UpdateProduct = async (req, res) => {
     const { ObjectId } = mongoose.Types;
     const method = req.method
     const { id } = req.params
-    const data = req.body
+    let data = req.body
     const files = req.files
     const imgs = files.map(item=>item?.path)
+    
     //check id valid
     if(!ObjectId.isValid(id)){
         return res.status(404).send({message: "Id is not a valid"})
@@ -26,7 +27,7 @@ const UpdateProduct = async (req, res) => {
         case "PATCH":
             try {
                 const {categoryId} = data
-                let {error} =  SchemaProduct.validate(data)
+                let {error} =  SchemaProductUpdate.validate(data)
                 if(error){
                     return res.status(400).send({message:error.message});
                 }
@@ -34,9 +35,21 @@ const UpdateProduct = async (req, res) => {
                 if (!product) {
                     return res.status(404).send({ message: "Product not found" });
                 }
+                const arrayImg = product.imgs.map(item=>{
+                    return "products/"+item.split("/").pop().replace('.png', '');
+                })                
                 const isCate = await Categories.findOne({_id: categoryId})
                 if(!isCate) {
                     return res.status(404).send({ message: "Categories not found" });
+                }
+
+                if(imgs.length) {
+                    data = {
+                        ...data,
+                        img: imgs[0],
+                        imgs:imgs
+                    }
+                    cloudinary.api.delete_resources(arrayImg)
                 }
                 const productUpdate = await Product.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: data }, { new: true , useFindAndModify: false})
                 await Categories.findByIdAndUpdate(categoryId, {
---
+++ src/validate/SchemaProductUpdate.js
@@ -0,0 +1,22 @@
+import Joi from "joi"
+
+const SchemaProductUpdate = Joi.object({
+    name: Joi.string().required().min(6).message({
+        "string.empty": `name should not be empty`,
+        "string.min":"name should be at least 6 character"
+    }),
+    price: Joi.number().required().min(1).message({
+        "number.empty": `number Should not be empty`,
+        "number.min":"number should be > 1"
+    }),
+    categoryId: Joi.string().required(),
+    description:Joi.string().required(),
+    categoryId: Joi.string().required(),
+    discount: Joi.number().min(1).message({
+        "string.min": "discount should be > 1"
+    }),
+    imgs: Joi.array()
+
+    
+})
+export default SchemaProductUpdate
